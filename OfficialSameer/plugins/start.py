4d7374ecab204d7968cafde13491b7fa6bedd517
garwmishra <85062023+garwmishra@users.noreply.github.com>
2022-04-09 18:38:00 +0530

    Update start.py

--- OfficialSameer/plugins/start.py
+++ OfficialSameer/plugins/start.py
@@ -5,24 +5,24 @@ from telethon import events, Button
 from telethon.tl.custom import button
 from .. import SAM, SAM2, SAM3, SAM4, SAM5, SAM6, SAM7, SAM8, SAM9, SAM10, ALIVE_PIC, OWNER_ID
 
-DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/c6f99c0b68ff07439ed72.jpg"
+DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/be526220ab16ee5504652.jpg"
 
 Deadly_Button = [
         [
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Deadly_spam_bot")
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/kaal_network")
         ],
         [
-        Button.url("• ᴄᴍᴅs •", "https://telegra.ph/%F0%9D%97%A5%F0%9D%97%9C%F0%9D%97%AD%F0%9D%97%A2%F0%9D%97%98%F0%9D%97%9F-%F0%9D%97%AB-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0-11-28-2")
+        Button.url("• ᴄᴍᴅs •", "https://t.me/its_heaven_king")
         ]
         ]
                
 DeadlyX_Button = [
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Deadly_spambot"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Deadly_spam_bot")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/kaal_network"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/heaven_army")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/Godfatherakkii/SAVAGE-BOTFATHER-SPAM")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/garwmishra/terabaapkaal")
         ]
         ]
         
@@ -49,8 +49,8 @@ async def start(event):
        replied_user = await event.client(GetFullUserRequest(event.sender_id))
        TheDeadly = event.chat_id
        firstname = replied_user.user.first_name
-       ownermsg = f"**Hi Master, Its me {bot_id}, Your Spam Bot !! \n\n Click Below Buttons For help**"
-       usermsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**"
+       ownermsg = f"**Hi Master, Its me {bot_id}, Your Kaal Spam Bot !! \n\n Click Below Buttons For help**"
+       usermsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own kaal Spam Bots You Can Deploy From Button Below.** \n\n**"
        if event.sender_id == OWNER_ID:
             await event.client.send_file(TheDeadly,
                   DEADLY_IMG,
